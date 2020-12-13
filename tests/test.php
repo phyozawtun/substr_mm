eed6370d30ffbd0f638b643a4afb45df8f4248ff
User <user@PC69.local>
2020-12-14 06:18:06 +0630

    Debug composer config

--- tests/test.php
+++ tests/test.php
@@ -6,7 +6,7 @@ use substr_mm\substr_mm;
 
 $string = 'သီဟိုဠ်မှ ဉာဏ်ကြီးရှင်သည် အာယုဝဍ္ဎနဆေးညွှန်းစာကို ဇလွန်ဈေးဘေး ဗာဒံပင်ထက် အဓိဋ္ဌာန်လျက် ဂဃနဏဖတ်ခဲ့သည်။';
 $start = 0;
-$length = 20;
+$length = 19;
 $output = substr_mm::substr_mm($string, $start, $length);
-echo $output;
+echo $output."...";
 ?>
